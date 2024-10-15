48f986646375b364dd0e03117c93d6067f4d9011
b1ek <me@blek.codes>
2024-10-15 12:10:43 +1000

    fix a ts error

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,6 +3,6 @@ import type { RequestEvent } from "@sveltejs/kit";
 export async function load(data: RequestEvent) {
     return {
         origin: `${data.url.protocol}//${data.url.hostname}:${data.url.port}`,
-        csrf: 'kit.session=' + encodeURIComponent(data.cookies.get('kit.session'))
+        csrf: 'kit.session=' + encodeURIComponent(data.cookies.get('kit.session') ?? '')
     }
 }
