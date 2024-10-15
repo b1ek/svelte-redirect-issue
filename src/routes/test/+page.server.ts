f7b55ed42ddaadcdfd903ec9440de71b7f62b204
b1ek <me@blek.codes>
2024-10-15 12:13:19 +1000

    ssr and prerender for all pages

--- src/routes/test/+page.server.ts
+++ src/routes/test/+page.server.ts
@@ -1,10 +1,8 @@
 import { base } from "$app/paths";
 import { redirect, type RequestEvent } from "@sveltejs/kit";
 
+export const prerender = false;
+
 export const actions = {
     default: async () => redirect(302, base + '/it-worked')
-}
-
-export function load(data: RequestEvent) {
-    return redirect(302, base + '/it-worked');
 }
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,1 @@
+you need to open this page via a form for the thing to working
